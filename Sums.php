0e20b5f1ee031118d8ea3b224069cce6e653f4f3
tomaszjureczko <tomasz.jureczko@gmail.com>
2013-08-12 20:53:58 +0100

    another variation

--- Sums.php
+++ Sums.php
@@ -13,27 +13,20 @@ $decimal = 0;
 
 function convertRomanToDecimal($roman)
 {
-    global $decimal;
-
-    addFirstRomanDigit($roman);
-
-    return $decimal;
-}
-
-function addFirstRomanDigit($roman)
-{
     global $convertingMap, $decimal;
 
     $currentDecimal = $convertingMap[$roman[0]];
 
     if (isset($roman[1])) {
-        addFirstRomanDigit(substr($roman, 1));       
+        convertRomanToDecimal(substr($roman, 1));       
         $nextDecimal = $convertingMap[$roman[1]];
         if ($nextDecimal > $currentDecimal) {
             $decimal -= $currentDecimal;
-            return;
+            return $decimal;
         }
     }
 
     $decimal += $currentDecimal;
+
+    return $decimal;
 }
